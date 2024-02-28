d39eec587d389063e8e86887a05756de9f0344fd
Asher Spurr <asher.spurr@gmail.com>
2024-02-28 14:20:25 -0700

    feature/use fetched data in test func

---
+++ src/apiCalls.js
@@ -0,0 +1,27 @@
+
+import { sortTrips } from "./scripts"
+
+function getData() {
+
+  const user = fetch('http://localhost:3001/api/v1/travelers/30')
+    .then(resp => resp.json())
+
+  const trips = fetch('http://localhost:3001/api/v1/trips')
+    .then(resp => resp.json())
+
+  const destinations = fetch('http://localhost:3001/api/v1/destinations') 
+    .then(resp => resp.json())
+    
+  Promise.all([user, trips, destinations])
+    .then((data) => {
+      let [user, trips, destinations] = data
+      console.log('sorted trips', sortTrips(user, trips))
+      console.log('user', user)
+      console.log('trip', trips)
+      console.log('destinations', destinations)
+    })
+}
+
+export {
+  getData
+}
