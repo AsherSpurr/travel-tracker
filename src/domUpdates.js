5204e08ec885fb4c8632f7120dc1939ee3bcd1c0
Asher Spurr <asher.spurr@gmail.com>
2024-02-28 18:40:48 -0700

    feature: pass userTrips test

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,11 +1,21 @@
 
-// const tripImages = document.getElementById('img-past')
+const modalPast = document.querySelector('#modal-past')
+const modalCurrent = document.querySelector('#modal-current')
 // const modalPast = document.querySelector('#modal-past')
 
-// tripImages.addEventListener('click', removeModal)
+document.querySelectorAll('.image').forEach(img => {
+  img.addEventListener('click', () => {
+    removeModal(img.id) 
+  })
+})
 
-// function removeModal() {
-//   console.log('trip image value', tripImages.id)
-//   modalPast.classList.remove('hidden')
-// }
+function removeModal(imgValue) {
+  if (imgValue === 'img-past') {
+    modalPast.classList.remove('hidden')
+  } 
+  if (imgValue === 'img-current') {
+    modalCurrent.classList.remove('hidden')
+  }
+  console.log('trip image value', imgValue)
+}
 
--- src/scripts.js
+++ src/scripts.js
@@ -4,33 +4,12 @@ import { getData } from './apiCalls';
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 // import './css/index.scss'
+import './domUpdates'
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import './images/loc1.jpg'
 import './images/loc2.jpg'
 import './images/loc3.jpg'
-
-/*<><><><><><><><><><><><><><><> DOM UPDATES <><><><><><><>><><><><><><><><> */
-const modalPast = document.querySelector('#modal-past')
-const modalCurrent = document.querySelector('#modal-current')
-// const modalPast = document.querySelector('#modal-past')
-
-document.querySelectorAll('.image').forEach(img => {
-  img.addEventListener('click', () => {
-    removeModal(img.id) 
-  })
-})
-
-function removeModal(imgValue) {
-  if (imgValue === 'img-past') {
-    modalPast.classList.remove('hidden')
-  } 
-  if (imgValue === 'img-current') {
-    modalCurrent.classList.remove('hidden')
-  }
-  console.log('trip image value', imgValue)
-}
 
 /* <><><><><><><><><><><><><><><> Javascript <><><><><><><><><><><><><><> */
 function userTrips(userData, tripsData) {
@@ -38,21 +17,10 @@ function userTrips(userData, tripsData) {
   let sortedTrips = trips.filter((trip) => {
     return userData.id === trip.userID
   })
+  console.log('sorted trips', sortedTrips)
   sortTrips(sortedTrips)
   return sortedTrips
 }
-
-// function sortPastTrips(trips) {
-//   return trips.filter((trip) => {
-//     return trip.status === 'approved'
-//   })
-// }
-
-// function sortCurrentTrips(trips) {
-//   return trips.filter((trip) => {
-//     return trip.status === 'pending'
-//   })
-// }
 
 function sortTrips(trips) {
   let pastTrips = trips.filter((trip) => {
@@ -68,5 +36,6 @@ function sortTrips(trips) {
 getData()
 
 export {
-  userTrips
+  userTrips,
+  sortTrips,
 }
